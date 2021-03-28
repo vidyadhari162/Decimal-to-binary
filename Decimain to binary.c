3ec8ae2c20d59fb17084d8ea579b42d93368bc8f
vidyadhari162 <81513977+vidyadhari162@users.noreply.github.com>
2021-03-28 17:18:18 +0530

    Add files via upload

---
+++ Decimain to binary.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int 
+main()
+
+{
+   
+long num, decimal_num, remainder, base=1, binary=0;
+ 
+   printf("Enter a decimal integer\n");
+  
+   scanf("%ld", &num);
+ 
+   decimal_num=num;
+  
+   while(num>0)
+    
+  {
+ 
+    remainder=num%2;
+ 
+    binary=binary+remainder*base;
+ 
+    num=num/2;
+  
+    base=base*10;
+ 
+    }
+   
+    printf("input number is %ld\n", decimal_num);
+  
+    printf("its binary equivalent is %ld\n", binary);
+ 
+   }
